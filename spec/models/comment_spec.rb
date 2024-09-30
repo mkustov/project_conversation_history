c652960bf31bc2fc515c0281d0e214139386f0d1
Maxim Kustov <maksym.kustov@gmail.com>
2024-09-30 21:30:51 +0100

    Adds comments model

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  let(:comment) { Comment.new(body: 'This is a comment', user: user, project: project ) }
+  let(:user) { User.create(email: 'gordon@blackmesa.com', password: 'HalfLife3') }
+  let(:project) { Project.create(title: 'The Crystal', description: 'Unknown origin', status: Project::VALID_STATUSES.first) }
+
+  it 'is valid with valid attributes' do
+    expect(comment).to be_valid
+  end
+
+  context 'validations' do
+    it 'validates the presence of body' do
+      comment.body = nil
+      expect(comment).to be_invalid
+    end
+  end
+end
